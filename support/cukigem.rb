61568a891851a8358d485ef4baefeaf1606c6e8c
Ben Hughes <ben@railsgarden.com>
2010-10-29 11:05:05 +0300

    Simplified Cukigem namespce to be a class with attr_accessor.

--- support/cukigem.rb
+++ support/cukigem.rb
@@ -1,17 +1,15 @@
 ENV["RAILS_ENV"] = "test"
 
-module Cukigem
-  require "active_support/core_ext"
-  
-  mattr_accessor :project_root, :temp_root, :application_name
-  
-  self.project_root = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).freeze
-  self.temp_root = File.join(project_root, "tmp").freeze
-  self.application_name = "rails_app".freeze
-  
+class Cukigem
   class << self
+    attr_accessor :project_root, :temp_root, :application_name
+    
     def app_root
       File.join(temp_root, application_name)
     end
   end
+  
+  self.project_root = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).freeze
+  self.temp_root = File.join(project_root, "tmp").freeze
+  self.application_name = "rails_app".freeze
 end
